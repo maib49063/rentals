058f790881406b918b36640f35a0f5be87307587
maib49063 <maib49063@gmail.com>
2026-05-21 14:39:04 +0500

    vkr0/3R

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,28 +3,32 @@ const router = express.Router();
 
 const { authenticateToken, authenticateAdmin } = require('../middlewares/auth.middleware');
 const upload = require('../middlewares/upload.middleware');
+// Подключаем наши новые валидаторы
+const { contactValidation, registerValidation, bookingValidation } = require('../middlewares/validator.middleware');
 
 const authCtrl = require('../controllers/auth.controller');
 const publicCtrl = require('../controllers/public.controller');
 const adminCtrl = require('../controllers/admin.controller');
 
-// Авторизация
-router.post('/auth/register', authCtrl.register);
+// Авторизация (навесили registerValidation)
+router.post('/auth/register', registerValidation, authCtrl.register);
 router.post('/auth/login', authCtrl.login);
 
 // Паблик
 router.get('/slider', publicCtrl.getSlider);
-router.post('/contact', publicCtrl.contact);
+// Навесили contactValidation
+router.post('/contact', contactValidation, publicCtrl.contact);
 router.get('/cars', publicCtrl.getCars);
-router.post('/bookings', authenticateToken, publicCtrl.bookCar);
+// Навесили bookingValidation
+router.post('/bookings', authenticateToken, bookingValidation, publicCtrl.bookCar);
 
 // Админка
 router.get('/admin/bookings', authenticateAdmin, adminCtrl.getBookings);
 router.put('/admin/bookings/:id/cancel', authenticateAdmin, adminCtrl.cancelBooking);
 router.get('/admin/cars', authenticateAdmin, adminCtrl.getCars);
 router.post('/admin/cars', authenticateAdmin, upload.single('image'), adminCtrl.addCar);
+router.put('/admin/cars/:id', authenticateAdmin, adminCtrl.updateCar); // Обновление цены/класса
 router.put('/admin/cars/:id/photo', authenticateAdmin, upload.single('image'), adminCtrl.updatePhoto);
 router.delete('/admin/cars/:id', authenticateAdmin, adminCtrl.deleteCar);
-router.put('/admin/cars/:id', authenticateAdmin, adminCtrl.updateCar);
 
 module.exports = router;
